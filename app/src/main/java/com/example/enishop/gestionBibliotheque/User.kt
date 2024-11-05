056bc25828814532736a9fa98335eecb9a9b89ba
mrichard12023 <mylan.richard2023@campus-eni.fr>
2024-11-05 11:44:17 +0100

    modif User.kt for correct

--- app/src/main/java/com/example/enishop/gestionBibliotheque/User.kt
+++ app/src/main/java/com/example/enishop/gestionBibliotheque/User.kt
@@ -3,8 +3,7 @@ package com.example.enishop.gestionBibliotheque
 class User(val name: String, val id: Int, val borrowedItems: MutableList<Item> = mutableListOf()) {
 
     fun borrow(item: Item): Boolean {
-        return if (item.isAvailable) {
-            item.isAvailable = false
+        return if (item is Borrowable && item.borrow(this)) {
             borrowedItems.add(item)
             println("${item.title} have been borrowed by $name.")
             true
@@ -15,8 +14,8 @@ class User(val name: String, val id: Int, val borrowedItems: MutableList<Item> =
     }
 
     fun returnItem(item: Item) {
-        if (borrowedItems.contains(item)) {
-            item.isAvailable = true
+        if (item is Borrowable && item in borrowedItems) {
+            item.returnItem(this)
             borrowedItems.remove(item)
             println("${item.title} has been returned by $name.")
         }
